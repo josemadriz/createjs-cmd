a03223d47d790ea70188d862cb35fbae3156e8c6
Jose Madriz <jose.madriz@possible.com>
2018-10-23 18:01:14 -0600

    wrapping app new version for webpack

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-require(
-    'imports-loader?this=>window!' +
-    './lib/easeljs-0.8.2.combined'
-);
-require(
-    'imports-loader?this=>window!' +
-    './lib/tweenjs-0.6.2.combined'
-);
-require(
-    'imports-loader?this=>window!' +
-    './lib/preloadjs-0.6.2.combined'
-);
+// require(
+//     'imports-loader?this=>window!' +
+//     './lib/easeljs-0.8.2.combined'
+// );
+// require(
+//     'imports-loader?this=>window!' +
+//     './lib/tweenjs-0.6.2.combined'
+// );
+// require(
+//     'imports-loader?this=>window!' +
+//     './lib/preloadjs-0.6.2.combined'
+// );
+// module.exports = require(
+//     'imports-loader?this=>window!' +
+//     'exports-loader?window.createjs!' +
+//     './lib/soundjs-0.6.2.combined'
+// );
+
 module.exports = require(
     'imports-loader?this=>window!' +
     'exports-loader?window.createjs!' +
-    './lib/soundjs-0.6.2.combined'
+    './builds/1.0.0/createjs.js'
 );
 
 
